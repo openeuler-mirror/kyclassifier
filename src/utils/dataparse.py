31a8b2478434271934c63eec1de052024fc0a275
dingsheng <dingsheng@kylinos.cn>
2024-09-05 03:24:33 +0000

    新增获取iso中pkginfo数据功能

--- src/utils/dataparse.py
+++ src/utils/dataparse.py
@@ -12,6 +12,7 @@
 # See the Mulan PSL v2 for more details.
 # **********************************************************************************
 """
+import copy
 import hawkey
 
 RPMINFO = {
@@ -57,3 +58,37 @@ class ISODataParse(object):
             else:
                 continue
         return pkgname_set
+    
+    @classmethod
+    def get_pkgsinfo_list(cls,files_path):
+        """
+            获取软件包信息列表
+        Args:
+            files_path (tuple): ISO数据文件路径
+        Returns:
+            res : iso中pkginfo列表
+        """
+        if not isinstance(files_path,tuple) or len(files_path) != 3:
+            return []
+        res = []
+        sack = hawkey.Sack()
+        repo = hawkey.Repo("repo_parse")
+        repo.repomd_fn,repo.primary_fn,repo.filelists_fn = files_path
+        sack.load_repo(repo,load_filelists=True)
+        q = hawkey.Query(sack)
+        for p in q:
+            pkginfo = copy.copy(RPMINFO)
+            pkginfo['name'] = p.name
+            pkginfo['arch'] = p.arch
+            pkginfo['version'] = p.version
+            pkginfo['epoch'] = str(p.epoch)
+            pkginfo['release'] = p.release
+            pkginfo['summary'] = p.summary
+            pkginfo['description'] = p.description
+            pkginfo['url'] = p.url
+            pkginfo['rpm_license'] = p.license
+            pkginfo['rpm_vendor'] = p.vendor
+            pkginfo['rpm_group'] = p.group
+            pkginfo['rpm_sourcerpm'] = p.sourcerpm
+            res.append(pkginfo)
+        return res
