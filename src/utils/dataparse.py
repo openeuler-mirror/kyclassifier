e5d0aeff23989ebb50805eee92b1513f6e983216
dingsheng <dingsheng@kylinos.cn>
2024-09-06 06:34:14 +0000

    新增数据解析模块单包信息查询功能

--- src/utils/dataparse.py
+++ src/utils/dataparse.py
@@ -30,10 +30,31 @@ RPMINFO = {
     'rpm_sourcerpm' : '',
 }
 
-class ISODataParse(object):
+class DataParse(object):
+    """
+        软件包数据解析模块
+    """
+    def __init__(self):
+        self.pkgs_name = set()
+        self.pkgs_info = []
+        
+    def get_pkginfo_byname(self):
+        pass
+
+class ISODataParse(DataParse):
     """
         ISO数据解析模块
     """
+    def __init__(self,files_path):
+        """
+        Args:
+            files_path (tuple): ISO数据文件路径
+        """
+        super(ISODataParse,self).__init__()
+        self.pkgs_name = self.get_pkgname_set(files_path)
+        self.pkgs_info = self.get_pkgsinfo_list(files_path)
+        self.pkgname_pkginfo_dict = self._list2dict(self.pkgs_info,"name")
+
     @classmethod
     def get_pkgname_set(cls,files_path):
         """
@@ -91,4 +112,33 @@ class ISODataParse(object):
             pkginfo['rpm_group'] = p.group
             pkginfo['rpm_sourcerpm'] = p.sourcerpm
             res.append(pkginfo)
+        return res
+
+    def get_pkginfo_byname(self,pkgname):
+        """
+            通过pkgname获取pkginfos
+        Args:
+            pkgname (str)
+        Returns:
+            pkginfos (list) [info1,info2]
+        """
+        return self.pkgname_pkginfo_dict.get(pkgname)
+
+    @classmethod
+    def _list2dict(cls, dict_list, key):
+        """
+            数据格式转换list2dict
+        Args:
+            dict_list (list): [d1,d2,...]
+            key (str): dict 中作为索引的key
+        Returns:
+            res (dict): {d[key]:[d1,d2,...], ...}
+        """
+        res = {}
+        for d in dict_list:
+            v = d.get(key)
+            if v not in res.keys():
+                res[v] = [d]
+            else:
+                res[v].append(d)
         return res
