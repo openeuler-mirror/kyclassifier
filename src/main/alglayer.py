fefb96189c6f020b278c5476e929dcd7961e7645
dingsheng <dingsheng@kylinos.cn>
2024-09-12 05:52:39 +0000

    新增分层数据处理

--- src/main/alglayer.py
+++ src/main/alglayer.py
@@ -59,3 +59,50 @@ class AlgLayer(object):
         for k,v in id2pkgs_dict.items():
             res_dict[k] = list(set(v) & set(all_pkgs_set))
         return res_dict
+
+    @classmethod
+    def augment_layered_set(cls,id2pkgs_dict,dep_obj,add_no_depby_pkgs=True):
+        """
+            补充分层集合,把无被依赖软件包加入第4层;把每层的依赖包加入各层级；
+        Args:
+            id2pkgs_dict (dict): 输入分层字典
+            dep_obj : DepParse类对象
+            add_no_depby_pkgs (bool, optional): 控制是否将无被依赖软件包加入第4层,默认为True . Defaults to True.
+
+        Returns:
+            id2pkgs_dict: 输出分层字典
+        """
+        dep_by_dict = dep_obj.dep_by_dict
+        if add_no_depby_pkgs:
+            for k,v in dep_by_dict.items():
+                if not v:
+                    id2pkgs_dict[4].append(k)
+        else:
+            for id,p_list in copy.deepcopy(id2pkgs_dict).items():
+                for p in p_list:
+                    p_deps = dep_obj.get_pkg_all_deps(p)
+                    id2pkgs_dict[id] += p_deps
+                id2pkgs_dict[id] = list(set(id2pkgs_dict[id]))
+        return id2pkgs_dict
+
+    @classmethod
+    def filter_duplicates(cls,id2pkgs_dict):
+        """
+            处理有多个层级的软件包，只保留最低层级
+        Args:
+            id2pkgs_dict (dict): 输入分层字典
+
+        Returns:
+            res_dict: 输出分层字典
+        """
+        visited_pkglist =[]
+        res_dict = {k:[] for k in range(1,5)}
+        for k,v in id2pkgs_dict.items():
+            if k==1:
+                res_dict[k] = list(set(v))
+            else:
+                res_dict[k] = list(set(v) - set(visited_pkglist))
+            visited_pkglist += res_dict[k]
+            visited_pkglist = list(set(visited_pkglist))
+            res_dict[k] = list(set(res_dict[k]))
+        return res_dict
