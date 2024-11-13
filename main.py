7c0e1c0debca7d84b0305cabbc6ed76d345d28e6
dingsheng <dingsheng@kylinos.cn>
2024-11-13 03:14:57 +0000

    新增支持仓库软件包分层分类功能

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ import os
 import shutil
 import json
 import argparse
+
 from src.utils.isocheck import IsoCheck
+from src.utils.repocheck import RepoCheck
 from src.utils import dataparse
 from src.utils import depparse
 from src.utils import util
@@ -37,6 +39,14 @@ class kyClassifier(object):
         files_path = ISOUtils.parase_iso_repofile(iso_path)
         depobj = depparse.ISODepParse(files_path)
         dataobj = dataparse.ISODataParse(files_path)
+        layer_res = AlgLayer.run(depobj,LAYERDATA)
+        classify_res =AlgClassify.run(dataobj,CLASSIFYDATA)
+        cls.save_output(layer_res,classify_res)
+
+    @classmethod
+    def process_repo(cls):
+        depobj = depparse.RepoDepParse()
+        dataobj = dataparse.RepoDataParse()
         layer_res = AlgLayer.run(depobj,LAYERDATA)
         classify_res =AlgClassify.run(dataobj,CLASSIFYDATA)
         cls.save_output(layer_res,classify_res)
@@ -65,10 +75,15 @@ if __name__ == '__main__':
     str_usage = 'kyclassifier ' + ' '.join(options)
     parser = argparse.ArgumentParser(usage=str_usage)
     parser.add_argument('-iso', type=str, help='Input ISO file path')
+    parser.add_argument('-repo', action = 'store_true', help='Whether to analyze repo packages.')
     args = parser.parse_args()
     if args.iso:
         if not IsoCheck.check(args.iso):
             sys.exit(1)
         kyClassifier.process_iso(args.iso)
+    if args.repo:
+        if not RepoCheck.check():
+            sys.exit(1)
+        kyClassifier.process_repo()   
 
 
