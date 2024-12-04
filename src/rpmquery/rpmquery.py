dba5a9821d1c9b66e694853c79932fa7aecef23a
dingsheng <dingsheng@kylinos.cn>
2024-12-04 09:15:47 +0000

    Init RpmQuery and RpmCheck module.

---
+++ src/rpmquery/rpmquery.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+# **********************************************************************************
+# Copyright (c) KylinSoft Co., Ltd. 2024.All rights reserved.
+# [kyclassifier] licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+# **********************************************************************************
+"""
+
+import rpm
+
+class RpmQuery(object):
+    """
+        rpm查询模块
+    """
+
+    def __init__(self):
+        pass
+
+    def get_rpm_layer(self):
+        """
+            查询rpm层级
+        """
+        pass
+    
+    def get_rpm_category(self):
+        """
+            查询rpm类别
+        """
+        pass
+
+    def _get_rpminfo(self,rpmpath):
+        """
+            获取rpminfo 
+        """
+        # Rpmcheck 
+        ts = rpm.TransactionSet("/",rpm._RPMVSF_NOSIGNATURES)
+        hdr = ts._f2hdr(rpmpath)
+        # process hdr data
+        rpminfo = {}
+        return rpminfo
