0be42b6133b27e28c36a5da9f5c17a43784fb912
xieyanlong <xieyanlong@kylinos.cn>
2024-12-05 14:29:22 +0800

    Implement define of class object RpmHdrInfo attribute method

--- src/rpmquery/rpmhdrinfo.py
+++ src/rpmquery/rpmhdrinfo.py
@@ -15,10 +15,11 @@
 
 
 class RpmHdrInfo():
-    """
-        RpmHdrInfo 数据类
+    """RpmHdrInfo 数据类
     """
     def __init__(self, rpmhdr):
+        self.rpmhdr = rpmhdr
+
         self.name = rpmhdr['name']
         self.version = rpmhdr['version']
         self.release = rpmhdr['release']
@@ -28,31 +29,82 @@ class RpmHdrInfo():
         self.license = rpmhdr['license']
         self.url = rpmhdr['url']
 
+    def __repr__(self):
+        return f"<RpmHdrInfo({self.name}-{self.version}-{self.release}.{self.arch})>"
+
     def as_dict(self):
-        """
-            类属性字段转换为字典
+        """类属性字段转换为字典
+
         Returns:
             dict: 类属性字段转换的字典数据
         """
-        pass
+        return {
+            'name' : self.name,
+            'arch' : self.arch,
+            'version' : self.version,
+            'epoch' : self.epoch,
+            'release' : self.release,
+            'summary' : self.summary,
+            'description' : self.description,
+            'url' : self.url,
+            'license': self.license,
+            'vendor' : self.vendor,
+            'group' : self.group,
+            'sourcerpm' : self.rpm_sourcerpm,
+            'requires' : self.requires
+        }
 
     @property
     def epoch(self):
-        pass
+        """获取epoch对象的值
+
+        Returns:
+            str: epoch对应的值或空值
+        """
+        if hasattr(self.rpmhdr, "epoch"):
+            return self.rpmhdr["epoch"]
+        return None
 
     @property
     def vendor(self):
-        pass
+        """获取vendor对象的值
+
+        Returns:
+            str: vendor对应的值或空值
+        """
+        if hasattr(self.rpmhdr, "vendor"):
+            return self.rpmhdr["vendor"]
+        return None
 
     @property
     def group(self):
-        pass
+        """获取group对象的值
+
+        Returns:
+            str: group对应的值或空值
+        """
+        if hasattr(self.rpmhdr, "group"):
+            return self.rpmhdr["group"]
+        return ""
     
     @property
     def rpm_sourcerpm(self):
-        pass
+        """获取sourcerpm对象的值
+
+        Returns:
+            str: sourcerpm对应的值或空值
+        """
+        if hasattr(self.rpmhdr, "sourcerpm"):
+            return self.rpmhdr["sourcerpm"]
+        return ""
 
     @property
     def requires(self):
-        pass
+        """获取requires对象的值
 
+        Returns:
+            str: requires对应的值或空值
+        """
+        if hasattr(self.rpmhdr, "requires"):
+            return self.rpmhdr["requires"]
+        return []
