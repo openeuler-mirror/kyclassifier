1d2e63d95b88a9d9449d2e995aef075f8a70e97f
dingsheng <dingsheng@kylinos.cn>
2024-12-05 07:35:32 +0000

    Init QueryLayerInIso module.

---
+++ src/rpmquery/querylayeriniso.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+# **********************************************************************************
+# Copyright (c) KylinSoft Co., Ltd. 2024.All rights reserved.
+# [kyclassifier] licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+# **********************************************************************************
+"""
+
+from rpmquery import RpmQuery
+
+
+class QueryLayerInIso(RpmQuery):
+
+    def __init__(self,rpm,iso):
+        pass
+
+    def get_rpm_layer(self):
+        """查询rpm层级
+        """
+        pass
+
+    def _get_rpmdeps(self):
+        """获取rpm包在iso中的南向依赖包
+        """
+        pass
+
+    def _get_isopkgs_layer(self):
+        """获取isopkgs的分层结果
+        """
+        pass
+
+    def _get_isofiles(self):
+        """获取iso元数据
+        """
+        pass
+
+    def _check(self):
+        """检查输入rpm、iso文件
+        """
+        pass
+
+    @property
+    def rpm(self):
+        pass
+
+    @property
+    def iso(self):
+        pass
+
